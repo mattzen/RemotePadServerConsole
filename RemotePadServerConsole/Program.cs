5590f5a66ec7a22d082c2e1fbaad579effd17c1e
Mateusz Zmijewski <mattzy90@hotmail.com>
2019-11-09 01:07:00 -0500

    mouse manipulation artifacts added

--- RemotePadServerConsole/Program.cs
+++ RemotePadServerConsole/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace RemotePadServerConsole
@@ -39,8 +40,56 @@ namespace RemotePadServerConsole
 
         static void Main(string[] args)
         {
-            StartListener();
+            //StartListener();
+            Win32.POINT p = new Win32.POINT();
+
+
+            Win32.POINT l;
+            Win32.GetCursorPos(out l);
+
+            Win32.SetCursorPos(l.x + 200, l.y +200);
+
+            //Process pr = new Process();
+            //pr.StartInfo.FileName = "visual studiocmd";
+       
+            //pr.StartInfo.UseShellExecute = false;
+            //pr.StartInfo.CreateNoWindow = false;
+
+            //pr.Start();
+
             Console.WriteLine("Hello World!");
+
+
+
+
         }
+
+
+        public class Win32
+        {
+            [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+            public static extern IntPtr FindWindow(string lpClassName,
+                string lpWindowName);
+
+            // Activate an application window.
+            [DllImport("USER32.DLL")]
+            public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+            [DllImport("User32.Dll")]
+            public static extern long SetCursorPos(int x, int y);
+
+            [DllImport("user32.dll")]
+            public static extern bool GetCursorPos(out POINT lpPoint); 
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct POINT
+            {
+                public int x;
+                public int y;
+            }
+        }
+
+
     }
 }
